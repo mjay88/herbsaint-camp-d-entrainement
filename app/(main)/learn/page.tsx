b329daa18687e32c32c43f997ab6a69e738c1724
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-08-10 00:05:56 -0500

    main/learn page skeleton, component dependencies

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
+
+const LearnPage = async () => {
+const {isAuthenticated, redirectToSignIn} = await auth();
+
+if(!isAuthenticated){
+    return redirectToSignIn()
+}
+
+  return <div>Learn Page</div>;
+};
+
+export default LearnPage;
