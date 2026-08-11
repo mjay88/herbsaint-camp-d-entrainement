c9e9427491bc77cab862e139bc4fd2b768727b3a
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-08-11 17:56:02 -0500

    Update marketing footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,56 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button variant="ghost" className="flex-1">
+          <Image
+            src="/gumbo-circle.svg"
+            alt="Bowl of Gumbo"
+            height={58}
+            width={58}
+            className="mr-4 rounded-md"
+          />
+        </Button>
+        <Button variant="ghost" className="flex-1">
+          <Image
+            src="/wine-red.svg"
+            alt="Red wine Bottle"
+            height={78}
+            width={78}
+            className="mr-4 rounded-md"
+          />
+        </Button>
+        <Button variant="ghost" className="flex-1">
+          <Image
+            src="/spaghetti.svg"
+            alt="Spaghetti"
+            height={58}
+            width={58}
+            className="mr-4 rounded-md"
+          />
+        </Button>
+        <Button variant="ghost" className="flex-1">
+          <Image
+            src="/wholer-ham.svg"
+            alt="Ham Bone"
+            height={58}
+            width={58}
+            className="mr-4 rounded-md"
+          />
+        </Button>
+        <Button variant="ghost" className="flex-1">
+          <Image
+            src="/glass-wine.svg"
+            alt="Wine Glass"
+            height={58}
+            width={58}
+            className="mr-4 rounded-md"
+          />
+        </Button>
+      </div>
     </footer>
   );
 };
