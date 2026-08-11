2faaa7c83ad4af6f94fa534e110f59372907d434
Michael W Jarrett <mwilliamkjarrett@gmail.com>
2026-08-10 22:19:12 -0500

    Add learn page wrappers/continue layout for learn page (#166)

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -19,35 +19,28 @@ export const Sidebar = ({ className }: Props) => {
     >
       <Link href="learn">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-            <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
-            <h1 className="text-2xl font-extrabold text-orange-500 tracking-wide">
-                Herbsaint
-            </h1>
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-orange-500 tracking-wide">
+            Herbsaint
+          </h1>
         </div>
       </Link>
       <div className="flex flex-col gap-y-2 flex-1">
-        <SidebarItem 
-        label="Learn"
-        href="/learn"
-        iconSrc="/learn.svg"
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
         />
-           <SidebarItem 
-        label="Leaderboard"
-        href="/leaderboard"
-        iconSrc="/leaderboard.svg"
-        />
-           <SidebarItem 
-        label="Quests"
-        href="/quests"
-        iconSrc="/quests.svg"
-        />
+        <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
+          <SidebarItem label="Bodega" href="/shop" iconSrc="/shop.svg" />
       </div>
       <div className="p-4">
         <ClerkLoading>
-            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
         </ClerkLoading>
         <ClerkLoaded>
-            <UserButton />
+          <UserButton />
         </ClerkLoaded>
       </div>
     </div>
