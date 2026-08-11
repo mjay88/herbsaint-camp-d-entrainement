4342c177f6317444f638f4a012d9bef4fc303fd4
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-08-10 22:13:04 -0500

    Add learn page wrappers/continue layout for learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,14 +1,31 @@
 export const instant = false;
 import { auth } from "@clerk/nextjs/server";
+import { Header } from "./header";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { FeedWrapper } from "@/components/feed-wrapper";
 
 const LearnPage = async () => {
-const {isAuthenticated, redirectToSignIn} = await auth();
+  const { isAuthenticated, redirectToSignIn } = await auth();
 
-if(!isAuthenticated){
-    return redirectToSignIn()
-}
+  if (!isAuthenticated) {
+    return redirectToSignIn();
+  }
 
-  return <div>Learn Page</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ imageSrc: "/mascot.svg", title: "Menu" }}
+          hearts={5}
+          points={100}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Menu" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
