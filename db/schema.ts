37e644b96a154f03c375c606cb7c4b9daafed093
Michael W Jarrett <mwilliamkjarrett@gmail.com>
2026-08-11 22:52:59 -0500

    Add relations.ts, Update schema with userProgress (#168)

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,20 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const lessons = pgTable("lessons", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
 });
+
+export const userProgress = pgTable("userProgress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+  activeLessonId: integer("active_lesson_id").references(() => lessons.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5),
+  points: integer("points").notNull().default(0),
+});
+
+
