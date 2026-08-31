c4836476c6ad4150d4a7512bbfc6646bac350d1c
Michael W Jarrett <mwilliamkjarrett@gmail.com>
2026-08-31 12:38:05 -0500

    minor styling changes (#245)

--- app/lesson/curriculum-bubble.tsx
+++ app/lesson/curriculum-bubble.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const CurriculumBubble = ({ question }: Props) => {
   return (
-    <div className="flex items-center gap-x-4 mb-6">
+    <div className="flex flex-col-reverse lg:flex-row items-center gap-x-4 mb-6">
       <Image
         src="/mascot.svg"
         alt="Mascot"
@@ -17,14 +17,16 @@ export const CurriculumBubble = ({ question }: Props) => {
       <Image
         src="/mascot.svg"
         alt="Mascot"
-        height={40}
-        width={40}
+        height={150}
+        width={150}
         className="block lg:hidden"
       />
-      <div className="relative py-2 px-4 border-2 rounded-xl text-sm lg:text-base">
+      <div className="relative py-2 px-4 m-3 border-2 rounded-xl text-sm lg:text-base">
         {question}
-        <div className="absolute -left-3 top-1/3 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-y-1/2 rotate-90" />
+        <div className="absolute hidden lg:block -left-6 top-1/3 w-3 h-3 border-x-[16px] border-x-transparent border-t-16 transform -translate-y-1/2 rotate-90" />
+         <div className="absolute block lg:hidden -bottom-4 right-1/3 w-3 h-3 border-x-[16px] border-x-transparent border-t-16 transform -translate-x-1/2" />
       </div>
+      
     </div>
   );
 };
