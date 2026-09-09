09a3faf74b0baa59ebbdce32f0048c1d2cbb541f
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-09-08 19:51:58 -0500

    working on bug

--- app/lesson/[lessonId]/lessonId-content.tsx
+++ app/lesson/[lessonId]/lessonId-content.tsx
@@ -1,30 +1,27 @@
-import {getLesson, getUserProgress } from "@/db/queries";
+import { getLesson, getUserProgress } from "@/db/queries";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { Quiz } from "../quiz";
 
 /**
- * indexed access type to use just params in child component. 
+ * indexed access type to use just params in child component.
  * Deriving the type this way guarantees the childs prop type stays in sync actual route typing
  * If the route path ever changes, it won't break
  */
 
 type Props = {
-    params: PageProps<"/lesson/[lessonId]">["params"];
-}
+  params: PageProps<"/lesson/[lessonId]">["params"];
+};
 
-export const LessonIdContent = async ({params}: Props) => {
+export const LessonIdContent = async ({ params }: Props) => {
   const { userId, isAuthenticated, redirectToSignIn } = await auth();
-  const {lessonId} = await params;
+  const { lessonId } = await params;
   if (!isAuthenticated) {
     return redirectToSignIn();
   }
   const userProgress = await getUserProgress(userId);
-  
-  const lesson = await getLesson(
-    userId,
-    +lessonId,
-  );
+
+  const lesson = await getLesson(userId, +lessonId);
 
   if (!lesson || !userProgress) {
     redirect("/learn");
@@ -42,7 +39,7 @@ export const LessonIdContent = async ({params}: Props) => {
       initialLessonChallenges={lesson.challenges}
       initialHearts={userProgress.hearts}
       initialPercentage={initialPercentage}
+      initialUserProgress={userProgress}
     />
   );
 };
-
